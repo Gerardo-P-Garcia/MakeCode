13048f0de3a43a9e0f816a1831733ee8a9cf01cc
Gerardo-P-Garcia <60496043+Gerardo-P-Garcia@users.noreply.github.com>
2020-11-09 13:36:38 -0500

    Initial upload with comments

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+# Blinking hearts (from MakeCode tutorial)
 
+# Start instructions
+def on_forever():
+    # Show a heart icon
+    basic.show_icon(IconNames.HEART)
+
+    # Clear the screen
+    basic.clear_screen()
+
+    # Wait x milliseconds
+    basic.pause(500)
+
+    # Show a small heart icon
+    basic.show_icon(IconNames.SMALL_HEART)
+
+    # Clear the screen
+    basic.clear_screen()
+
+    # Wait x milliseconds
+    basic.pause(500)
+# End of instructions
+
+# Run the instructions on a loop without end
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+//  Blinking hearts (from MakeCode tutorial)
+//  Start instructions
+//  End of instructions
+//  Run the instructions on a loop without end
+basic.forever(function on_forever() {
+    //  Show a heart icon
+    basic.showIcon(IconNames.Heart)
+    //  Clear the screen
+    basic.clearScreen()
+    //  Wait x milliseconds
+    basic.pause(500)
+    //  Show a small heart icon
+    basic.showIcon(IconNames.SmallHeart)
+    //  Clear the screen
+    basic.clearScreen()
+    //  Wait x milliseconds
+    basic.pause(500)
+})
